afddbf629961e6c537aeb3dcf0e89687e2f9ca9d
santiagoSuarez219 <santiago8628@gmail.com>
2023-11-28 09:57:00 -0500

    Refactor main.py to use PyQt5 for GUI and add support for MQTT communication

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
-from ComunicacionSerial import ComunicacionSerial
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QPushButton, QComboBox, QLabel
+from PyQt5.QtCore import Qt
+from comunicacion_serial import comunicacion_serial
+from comunicacion_mqtt import comunicacion_mqtt
 
-vid_sensor = 0x1A86
-pid_sensor = 0x7523
-baud_rate_sensor = 115200
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Interfaz para sensor")
+        self.setGeometry(100, 100, 400, 200)
 
-comunicacion_serial = ComunicacionSerial(vid_sensor, pid_sensor, baud_rate_sensor)
-conexion = comunicacion_serial.conectar()
+        self.comunicacion_serial = comunicacion_serial(vid=0x1A86, pid=0x7523, baudrate=115200)
+        self.conexion_serial = self.comunicacion_serial.conectar()
 
-dato_a_enviar = "o"
+        if(not self.conexion_serial):
+            self.comunicacion_mqtt = comunicacion_mqtt(broker=" 192.168.1.103", port=1883)
+            
 
-# Convertir el string a bytes antes de enviarlo
-dato_a_enviar_bytes = dato_a_enviar.encode('utf-8')
-
-# Enviar el dato
-conexion.write(dato_a_enviar_bytes)
-
-# Leer la respuesta
-respuesta = conexion.readline().decode('utf-8')
-print(respuesta)
-
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
