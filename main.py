8834b4ad108951744faf7ffd7955d1801b5bae35
santiagoSuarez219 <santiago8628@gmail.com>
2023-11-28 12:36:26 -0500

    Add real-time data updating to the graph

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import time
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
 from PyQt5.QtWidgets import QApplication, QMainWindow
@@ -7,10 +8,21 @@ from comunicacion_serial.comunicacion_serial import comunicacion_serial
 from comunicacion_mqtt.comunicacion_mqtt import comunicacion_mqtt
 
 class Hilo(QThread):
-    def __init__(self):
+    def __init__(self, grafica):
         super(Hilo, self).__init__()
+        self.grafica = grafica
+        self.comunicacion_serial = comunicacion_serial(vid=0x1A86, pid=0x7523, baudrate=115200)
+        self.comunicacion_serial.conectar()
+        self.data = self.comunicacion_serial.leer()
     def run(self):
-        pass
+        while True:
+            self.actualizar_datos()
+            time.sleep(.050)
+    def actualizar_datos(self):
+        self.grafica.g1.clear()
+        self.data = self.comunicacion_serial.leer()
+        self.grafica.g1.imshow(self.data, cmap='jet')
+        self.grafica.draw()
 
 class grafica(FigureCanvasQTAgg):
     def __init__(self):
@@ -19,18 +31,11 @@ class grafica(FigureCanvasQTAgg):
         
         self.g1 = figura.add_subplot(111)
 
-
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setWindowTitle("Interfaz para sensor")
-        self.comunicacion_serial = comunicacion_serial(vid=0x1A86, pid=0x7523, baudrate=115200)
-        self.comunicacion_serial.conectar()
         self.mostrar_grafica()
-
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.actualizar_datos)
-        self.timer.start(100)
 
         # self.conexion_serial = self.comunicacion_serial.conectar()
 
@@ -40,13 +45,9 @@ class MainWindow(QMainWindow):
     def mostrar_grafica(self):
         self.grafica = grafica()
         self.setCentralWidget(self.grafica)
-        self.data = self.comunicacion_serial.leer()
 
-    def actualizar_datos(self):
-        self.grafica.g1.clear()
-        self.data = self.comunicacion_serial.leer()
-        self.grafica.g1.imshow(self.data, cmap='jet')
-        self.grafica.draw()
+        self.hilo = Hilo(self.grafica)
+        self.hilo.start()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
