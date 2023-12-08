f3bdccc17de464fe060935ef1bb3191c78de5421
santiagoSuarez219 <santiago8628@gmail.com>
2023-12-08 14:14:00 -0500

    Update UI layout and add temperature labels and save button

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QFrame, QHBoxLayout, QWidget, QGridLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QFrame, QHBoxLayout, QPushButton
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont
 from visualizacion_datos.visualizacion_datos import Grafica, ImagenTermica
@@ -14,7 +14,7 @@ class MainWindow(QMainWindow):
         self.setGeometry(200, 200, 1300, 500)
         self.display_widgets()
         contenedor = QFrame()
-        contenedor.setLayout(self.glyt_principal)
+        contenedor.setLayout(self.vlyt)
         self.setCentralWidget(contenedor)
     
     def display_widgets(self):
@@ -24,14 +24,38 @@ class MainWindow(QMainWindow):
         label.resize(800, 50)
         label.setAlignment(Qt.AlignCenter)
 
-        self.glyt_principal = QGridLayout(self)
-        self.glyt_principal.addWidget(label, 0, 0, 1, 2)
+        self.vlyt = QVBoxLayout()
+        self.vlyt.addWidget(label)
 
         self.grafica = Grafica()
         self.imagen_termica = ImagenTermica(self.grafica)   
         self.imagen_termica.start()
 
-        self.glyt_principal.addWidget(self.grafica, 1, 0, 1, 2)
+        self.vlyt.addWidget(self.grafica)
+
+        temperatura_maxima = QLabel(self)
+        temperatura_maxima.setText("Temperatura máxima: 35 °C")
+        temperatura_maxima.setFont(QFont("Arial", 10))
+
+        temperatura_minima = QLabel(self)
+        temperatura_minima.setText("Temperatura minima: 25 °C")
+        temperatura_minima.setFont(QFont("Arial", 10))
+
+        temperatura_promedio = QLabel(self)
+        temperatura_promedio.setText("Temperatura promedio: 30 °C")
+        temperatura_promedio.setFont(QFont("Arial", 10))
+
+        boton_guardar = QPushButton("Guardar")
+        boton_guardar.setFont(QFont("Arial", 10))
+        
+        xlyt = QHBoxLayout()
+        xlyt.addWidget(temperatura_maxima)
+        xlyt.addWidget(temperatura_minima)
+        xlyt.addWidget(temperatura_promedio)
+        xlyt.addWidget(boton_guardar)
+
+        self.vlyt.addLayout(xlyt)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
